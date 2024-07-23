c880410067e3c9172b74f564888b6011dc73e84e
Anthony Shew <anthonyshew@gmail.com>
2024-07-23 12:12:17 -0600

    Add a console.log to web app.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
+console.log("test");
+
 export default function Home() {
   return (
     <div className={styles.page}>
